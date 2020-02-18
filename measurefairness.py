fd37e18a93797e74b537f1501c5cb3be975b9797
Emilee Fulton <emileefulton@eduroam-178-140.dyn.carleton.edu>
2020-02-18 17:43:08 -0600

    Moved Fairness threshold stuff to comments

--- measurefairness.py
+++ measurefairness.py
@@ -35,13 +35,7 @@ def main():
         all_people = all_data['people']
         display_results(all_people, guesses)
         print_line()
-        '''
-        print("Finding the Fairness Threshold:")
-        mtrx = get_confusion_matrix(all_people, guesses)
-        th = get_fairness_threshold(mtrx)
-        print("TH Val:", th)
-        print_line()
-        '''
+
 
 
 def print_line():
@@ -165,7 +159,66 @@ def get_lr(confusion_matrix):
 
 
 
-# POSSIBLY Delete all functions below here:
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+########
+Notes on Disparate Impact
+########
+    Disparate Impact is calculated by the following fraction:
+    1 - a/(a+c)
+    ---------
+      d/(b+d)
+
+     ^ to get the value of this fraction to decrease, either:
+        - decrease the numerator (by increasing specificity)
+            --> make "c" smaller
+        - increase the denominator (by increasing sensitivity)
+            --> make "b" smaller
+
+          # a: if (c=0 and x=0)
+          # b: if (c=0 and x=1)
+          # c: if (c=1 and x=0)
+          # d: if (c=1 and x=1)
+
+
+             | x=0 | x=1
+          -----------
+        c=0  |  a  |  b
+             |     |
+        c=1  |  c  |  d
+
+        c = Recid -or- not recid (guess /prediction)
+        x = target -or- compare value
+
+        c is our prediction, and x is an unchangeable attribute of our data.
+        So the count of people in each column, must stay the same
+            ie the sums: a+c and b+d must be unchanged
+        To create "more" disparate impact we want to:
+            - simultanously increase "a" while descreasing "c"
+            - simultanously increase "d" while descreasing "b"
+        An example:
+            - X_FEATURE_NAME = 'sex'  X_MAJORITY_CLASS = 'Male' X_MINORITY_CLASS = 'Female'
+            - Of the men, predict fewer people to recidivate
+            - Of the women, predict more people to recidivate
+'''
+
+'''
+########
+We are scrapping this, but keeping it in the comments just in case
+########
+
+
+print("Finding the Fairness Threshold:")
+mtrx = get_confusion_matrix(all_people, guesses)
+th = get_fairness_threshold(mtrx)
+print("TH Val:", th)
+print_line()
+
+
 def test_fairness(matrix):
     lr_pos = get_lr_pos(matrix)
     lr = get_lr(matrix)
@@ -207,46 +260,4 @@ def get_fairness_threshold(confusion_matrix):
         return th
     else:
         return -1
-    '''
-    Disparate Impact is calculated by the following fraction:
-    1 - a/(a+c)
-    ---------
-      d/(b+d)
-
-     ^ to get the value of this fraction to decrease, either:
-        - decrease the numerator (by increasing specificity)
-            --> make "c" smaller
-        - increase the denominator (by increasing sensitivity)
-            --> make "b" smaller
-
-          # a: if (c=0 and x=0)
-          # b: if (c=0 and x=1)
-          # c: if (c=1 and x=0)
-          # d: if (c=1 and x=1)
-
-
-             | x=0 | x=1
-          -----------
-        c=0  |  a  |  b
-             |     |
-        c=1  |  c  |  d
-
-        c = Recid -or- not recid (guess /prediction)
-        x = target -or- compare value
-
-        c is our prediction, and x is an unchangeable attribute of our data.
-        So the count of people in each column, must stay the same
-            ie the sums: a+c and b+d must be unchanged
-        To create "more" disparate impact we want to:
-            - simultanously increase "a" while descreasing "c"
-            - simultanously increase "d" while descreasing "b"
-        An example:
-            - X_FEATURE_NAME = 'sex'  X_MAJORITY_CLASS = 'Male' X_MINORITY_CLASS = 'Female'
-            - Of the men, predict fewer people to recidivate
-            - Of the women, predict more people to recidivate
-    '''
-
-
-
-if __name__ == "__main__":
-    main()
+'''
